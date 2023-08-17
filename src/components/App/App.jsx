a2b5aab496f153fe75c6778a990a5ad1ca6a884c
Samuel Nelson <samuel.h.nelson@protonmail.com>
2023-08-17 11:49:09 -0500

    why nothing work

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import {useState, useEffect} from 'react';
+
 
 function App() {
+
+  const [pizzaList, setPizzaList] = useState([])
+
+  useEffect(() => {fetchPizzas}, [])
+
+const fetchPizzas = () => {
+  axios.get('/api/pizza')
+  .then(response => {
+  setPizzaList(response.data)
+  }).catch((error) => {
+  console.log('fetch failed:', error);  })
+}
+
+
 
   return (
     <div className='App'>
