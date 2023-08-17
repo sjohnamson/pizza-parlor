7b42cbf2d31ca2837a578c3f864ce9f754187b0e
Samuel Nelson <samuel.h.nelson@protonmail.com>
2023-08-17 13:12:28 -0500

    omg get wrking

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 
 function App() {
+  console.log("in appppppppp");
 
   const [pizzaList, setPizzaList] = useState([])
 
-  useEffect(() => {fetchPizzas}, [])
+  useEffect(() => { fetchPizzas() }, [])
 
-const fetchPizzas = () => {
-  axios.get('/api/pizza')
-  .then(response => {
-  setPizzaList(response.data)
-  }).catch((error) => {
-  console.log('fetch failed:', error);  })
-}
+  const fetchPizzas = () => {
+    axios.get('/api/pizza')
+      .then(response => {
+        setPizzaList(response.data)
+      }).catch((error) => {
+        console.log('fetch failed:', error);
+      })
+  }
 
 
 
@@ -25,10 +27,16 @@ const fetchPizzas = () => {
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
       </header>
-  
+
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
-  
+
+      <ul>
+        {pizzaList.map((pizza, index) =>
+          <li key={index}>{pizza.name}</li>
+        )}
+      </ul>
+
     </div>
   );
 }
