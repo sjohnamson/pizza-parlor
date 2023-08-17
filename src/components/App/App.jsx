c2cb7a7ede9ab3d9f975fbe388bbb8ee9b24e54a
Peter Ingles <peter.ingles@gmail.com>
2023-08-17 15:49:31 -0500

    Add RadioGroup for Pickup and Delivery

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,10 +3,38 @@ import axios from 'axios';
 import './App.css';
 import { useState, useEffect } from 'react';
 import PizzaList from '../PizzaList/PizzaList';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 
 
 function App() {
 
+  const [name, setName] = useState('')
+  const [streetAddress, setStreetAddress] = useState('')
+  const [city, setCity] = useState('')
+  const [zip, setZip] = useState('')
+
+  const [type, setType] = React.useState(null);
+
+  const handleTypeChange = (event) => {
+    event.preventDefault()
+    console.log("Type before", type, "event.target.value", event.target.value)
+    setType(event.target.value);
+    console.log("Type after", type, "event.target.value", event.target.value)
+  };
+ 
+  
+  
+
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log("inside handleSubmit")
+  }
   
 
 
@@ -21,6 +49,49 @@ function App() {
       <p>Pizza is great.</p>
 
       <PizzaList />
+
+      <form onSubmit={handleSubmit} className="add-customer-information-form">
+        <input 
+          required 
+          placeholder="Name" 
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+
+        <input 
+          required 
+          placeholder="Street Address" 
+          value={streetAddress}
+          onChange={(event) => setStreetAddress(event.target.value)}
+        />
+        <input 
+          required 
+          placeholder="City" 
+          value={city}
+          onChange={(event) => setCity(event.target.value)}
+        />
+        <input 
+          required 
+          placeholder="Zip" 
+          value={zip}
+          onChange={(event) => setZip(event.target.value)}
+        />
+        <div>
+        <FormControl>
+  <FormLabel id="demo-controlled-radio-buttons-group"></FormLabel>
+  <RadioGroup
+    aria-labelledby="demo-controlled-radio-buttons-group"
+    name="controlled-radio-buttons-group"
+    value={type}
+    onChange={handleTypeChange}
+  >
+    <FormControlLabel value="Pickup" control={<Radio />} label="Pickup" />
+    <FormControlLabel value="Delivery" control={<Radio />} label="Delivery" />
+  </RadioGroup>
+</FormControl>
+     
+      </div>
+        </form>
 
     </div>
   );
