8825dc6b750062ec773c82eccab200ef4e60e38f
Sam Johnson <s_johnso@yahoo.com>
2023-08-18 17:46:27 -0500

    added styling to the front page and header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,34 @@
 import { useSelector } from "react-redux"
+import { Link } from "react-router-dom";
+import { IconButton } from "@mui/material";
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Unstable_Grid2';
+import Stack from '@mui/material/Stack';
+import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout'
+import { Start } from "@mui/icons-material";
+
 
 function Header() {
     const cartTotal = useSelector(state => state.cartTotal);
 
     return (
         <header className='App-header'>
-            <h1 className='App-title'>Prime Pizza</h1>
-            <p>Cart Total: {cartTotal}</p>
+            <Stack direction="row" justifyContent="space-between">
+                <h1 className='App-title' >Pete's-a-Party & Sam-wiches</h1>
+                <Box>Cart Total: {cartTotal}
+                <Link to="customerform">
+                    <IconButton color="success">
+                        <ShoppingCartCheckoutIcon />
+                    </IconButton>
+                </Link>
+                </Box>
+            </Stack>
+
+
         </header>
+
+
     )
 }
 
--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -1,31 +1,21 @@
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import axios from 'axios';
-import { Link } from 'react-router-dom'
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Unstable_Grid2';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import RemoveShoppingCartIcon from '@mui/icons-material/RemoveShoppingCart';
 
 function PizzaList() {
 
     const [pizzaList, setPizzaList] = useState([]);
 
     const dispatch = useDispatch();
-
-    const Item = styled(Paper)(({ theme }) => ({
-        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-        ...theme.typography.body2,
-        padding: theme.spacing(1),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-    }));
 
     useEffect(() => { fetchPizzas() }, []);
 
@@ -54,11 +44,6 @@ function PizzaList() {
         <>
             <h1>Select Your Pizzas!</h1>
             <hr />
-            <Box sx={{
-                flexWrap: "wrap",
-            }}
-                justifyContent="center"
-                alignItems="center">
                 <Grid
                     container
                     rowGap={5}
@@ -68,42 +53,36 @@ function PizzaList() {
                     xs={12}>
                     {pizzaList.map((pizza, index) =>
 
-                        <Item>
-                            <Card sx={{ width: 300 }} key={index}>
-                                <CardMedia
-                                    sx={{ height: 200 }}
-                                    image={pizza.image_path}
-                                    title={pizza.name}
-                                />
-                                <CardContent
-                                    sx={{ height: 200 }}>
-                                    <Typography gutterBottom variant="h5" component="div">
-                                        {pizza.name}
-                                    </Typography>
-                                    <Typography variant="body1" color="text.secondary" component="div">
-                                        {pizza.price}
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary">
-                                        {pizza.description}
-                                    </Typography>
-                                </CardContent>
-                                <CardActions>
-                                    <Button size="small">Share</Button>
-                                    <Button size="small">Learn More</Button>
-                                </CardActions>
-
-                                <button onClick={() => addPizza(pizza)} >
-                                    Add to Cart
-                                </button>
-                                <button onClick={() => removePizza(pizza)}>Remove from Cart</button>
-
-                            </Card>
-                        </Item>
+                        <Card sx={{ width: 300 }} key={index}>
+                            <CardMedia
+                                sx={{ height: 200 }}
+                                image={pizza.image_path}
+                                title={pizza.name}
+                            />
+                            <CardContent
+                                sx={{ height: 200 }}>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    {pizza.name}
+                                </Typography>
+                                <Typography variant="body1" color="text.secondary" component="div">
+                                    {pizza.price}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    {pizza.description}
+                                </Typography>
+                            </CardContent>
+                            <CardActions sx={{ justifyContent: 'space-evenly' }}>
+                                <Button onClick={() => addPizza(pizza)} variant="contained" color="success" endIcon={<AddShoppingCartIcon />}>
+                                    Add
+                                </Button>
+                                <Button onClick={() => removePizza(pizza)} variant="contained" color="error" endIcon={<RemoveShoppingCartIcon />}>
+                                    Remove
+                                </Button>
+                            </CardActions>
+                        </Card>
 
                     )}
                 </Grid>
-            </Box>
-            <Link to="customerform"><h3><button>Next ➡️</button></h3></Link>
         </>
 
     )
