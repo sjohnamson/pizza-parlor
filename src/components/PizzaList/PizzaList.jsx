f6000dba48d81d5a45639c9d8e15329f197585e7
Sam Johnson <s_johnso@yahoo.com>
2023-08-18 16:35:04 -0500

    cards beginning build

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -2,12 +2,30 @@ import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import axios from 'axios';
 import { Link } from 'react-router-dom'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
 
 function PizzaList() {
 
     const [pizzaList, setPizzaList] = useState([]);
 
     const dispatch = useDispatch();
+
+    const Item = styled(Paper)(({ theme }) => ({
+        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        ...theme.typography.body2,
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    }));
 
     useEffect(() => { fetchPizzas() }, []);
 
@@ -36,23 +54,56 @@ function PizzaList() {
         <>
             <h1>Select Your Pizzas!</h1>
             <hr />
+            <Box sx={{
+                flexWrap: "wrap",
+            }}
+                justifyContent="center"
+                alignItems="center">
+                <Grid
+                    container
+                    rowGap={5}
+                    columnGap={5}
+                    alignItems="center"
+                    justifyContent="center"
+                    xs={12}>
+                    {pizzaList.map((pizza, index) =>
 
-            <div>
-                {pizzaList.map((pizza, index) =>
-                    <>
-                        <p key={index}>{pizza.name},{pizza.description}, {pizza.price}</p>
+                        <Item>
+                            <Card sx={{ width: 300 }} key={index}>
+                                <CardMedia
+                                    sx={{ height: 200 }}
+                                    image={pizza.image_path}
+                                    title={pizza.name}
+                                />
+                                <CardContent
+                                    sx={{ height: 200 }}>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        {pizza.name}
+                                    </Typography>
+                                    <Typography variant="body1" color="text.secondary" component="div">
+                                        {pizza.price}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        {pizza.description}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small">Share</Button>
+                                    <Button size="small">Learn More</Button>
+                                </CardActions>
 
-                        <button onClick={() => addPizza(pizza)} >
-                            Add to Cart
-                        </button>
-                        <button onClick={() => removePizza(pizza)}>Remove from Cart</button>
+                                <button onClick={() => addPizza(pizza)} >
+                                    Add to Cart
+                                </button>
+                                <button onClick={() => removePizza(pizza)}>Remove from Cart</button>
 
-                    </>
-                )}
+                            </Card>
+                        </Item>
 
-                <Link to="customerform"><h3><button>Next ➡️</button></h3></Link>
-
-            </div>
+                    )}
+                </Grid>
+            </Box>
+            <Link to="customerform"><h3><button>Next ➡️</button></h3></Link>
         </>
 
     )
